46b1e76ee94dfeda11d41e2ca589dad2c5895778
Djordy decroix <djordydecroix@gmail.com>
2025-09-04 13:17:58 +0200

    Ajout de la configuration des clés reCAPTCHA dans le fichier nuxt.config.ts pour gérer les variables côté serveur et côté client. Mise à jour des permissions de lecture dans settings.local.json pour inclure de nouveaux chemins d'accès aux fichiers, améliorant ainsi la gestion des ressources.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    // Variables côté serveur (privées)
+    recaptchaSecretKey: process.env.RECAPTCHA_SECRET_KEY,
+    
+    public: {
+      // Variables côté client (publiques)
+      recaptchaSiteKey: process.env.NUXT_PUBLIC_RECAPTCHA_SITE_KEY
+    }
+  },
+
   vite: {
     plugins: [
       tailwindcss(),
